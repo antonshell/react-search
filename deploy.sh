7255a2e9395f5c6964d91548d38df474c4a9e3d9
Anton Shelyganov <antonshell@yandex.ru>
2018-04-06 12:04:28 +0300

    Changes in deploy script. Removed unused code

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,2 @@
 npm run build
 cp -R proxy build/
-
-rm -rf /Users/antonshell/Projects/220v_search/public/web
-cp -r build /Users/antonshell/Projects/220v_search/public/web
-mkdir /Users/antonshell/Projects/220v_search/public/web/api
-cp /Users/antonshell/Projects/220v_search/public/index_front.php /Users/antonshell/Projects/220v_search/public/web/api/index.php
